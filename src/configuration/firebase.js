603d67047a5a769e9d834f7618bad7364917649e
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2026-09-22 23:25:40 -0300

    chore: project and firebase configuration

---
+++ src/configuration/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJHlg_rK-9JcZxN4g0Qm99nzZ3P5GZILw",
+  authDomain: "tech-web-af6.firebaseapp.com",
+  projectId: "tech-web-af6",
+  storageBucket: "tech-web-af6.firebasestorage.app",
+  messagingSenderId: "556268186267",
+  appId: "1:556268186267:web:6be56cc532f7d825e9343e"
+};
+
+export const app = initializeApp(firebaseConfig);
